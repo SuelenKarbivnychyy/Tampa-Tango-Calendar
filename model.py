9b205d064a8c60d71d39f48075ca2f1373a0583d
suelenk <suelenmatosr@gmail.com>
2022-11-03 11:35:55 -0400

    added relationship to data base tables, started creating hmtl pages

--- model.py
+++ model.py
@@ -1,6 +1,13 @@
-# ''' Model for Tango app'''
+# pseudocode
+# configure data base
+# create tables 
+# add relationship between tables
+
+
+"""Model for Tango app"""
 
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -19,51 +26,60 @@ class Event(db.Model):
     date = db.Column(db.DateTime)                        
     price = db.Column(db.Integer)                    
     location_id = db.Column(db.Integer, db.ForeignKey('locations.id'))
-    event_type_id = db.Column(db.Integer, db.ForeignKey('event_type.id'))
+    event_type_id = db.Column(db.Integer, db.ForeignKey('events_type.id'))
+
+    location = db.relationship("Location", back_populates="events")                             #specifing the relationship in between tables
+    event_type = db.relationship("Event_type", back_populates="events")                         #specifing the relationship in between tables
+    attendances = db.relationship("Attendance", back_populates="events")                        #specifing the relationship in between tables
 
     def __repr__(self):
         return f'<Event id={self.id} duration={self.duration} description={self.description}>'
 
 
 
-class Events_type(db.Model):
+class Event_type(db.Model):
     """A event type"""
 
-    __tablename__ = 'event_type'
+    __tablename__ = 'events_type'
 
     id = db.Column(db.Integer,
                     autoincrement = True,
                     primary_key = True)
     name = db.Column(db.String)
 
+    events = db.relationship("Event", back_populates="event_type")                       #specifing the relationship in between tables
+    
+
     def __repr__(self):
         return f'<Event_type id={self.id} name={self.name}>'
 
 
 
-class Locations(db.Model):
+class Location(db.Model):
     """A location"""
 
-    __tablename__ = 'location'
+    __tablename__ = 'locations'
 
     id = db.Column(db.Integer,
                     autoincrement = True,
                     primary_key = True)
     venue_name = db.Column(db.String(25))                                     
-    address = db.Column(db.Text(50))                        
+    address = db.Column(db.Text)                        
     city = db.Column(db.String(25))                        
     state = db.Column(db.String(25))                    
     zipcode = db.Column(db.Integer)
+
+    events = db.relationship("Event", back_populates="location")                        #specifing the relationship in between tables
 
     def __repr__(self):
         return f'<Locations id={self.id} venue_name={self.venue_name} address={self.address}>'
 
 
 
-class Users(db.Model):
+class User(db.Model):
     """A user"""
 
-    __classname__ = 'user'
+    __tablename__ = 'users'
 
     id = db.Column(db.Integer,
                     autoincrement = True,
@@ -74,38 +90,29 @@ class Users(db.Model):
     password = db.Column(db.String(30))                    
     is_adm = db.Column(db.Boolean)
 
+    attendances = db.relationship("Attendance", back_populates="user")                  #specifing the relationship in between tables
+
     def __repr__(self):
         return f'<User id={self.id} fname={self.fname} lname={self.lname} email={self.email}>'
 
 
 
-class Attendances(db.Model):
+class Attendance(db.Model):
     """A attendence"""
 
-    __tablename__ = 'event_type'
+    __tablename__ = 'attendances'
 
     id = db.Column(db.Integer,
                     autoincrement = True,
                     primary_key = True)
-    event_id = db.Column(db.Integer, db.ForeignKey('event.id'))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    event_id = db.Column(db.Integer, db.ForeignKey('events.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
+    events = db.relationship("Event", back_populates="attendances")                      #specifing the relationship in between tables
+    user = db.relationship("User", back_populates="attendances")                        #specifing the relationship in between tables
 
     def __repr__(self):
-        return f'<Event_type id={self.id} name={self.name}>'        
-
-
-
-
-                                                                                                  
-
-
-
-
-
-
-
-
+        return f'<Event_type id={self.id} name={self.name}>' 
 
 
 
@@ -121,13 +128,18 @@ def connect_to_db(app, db_name):
     db.app = app
     db.init_app(app)
 
+    print("Connected to the db!")
+
  
  
 
 if __name__ == "__main__":
     from server import app
 
-    connect_to_db(app, "tango-final-project")    
+    
+    connect_to_db(app, "tango-project") 
+
+     
 
 
 
--- server.py
+++ server.py
@@ -1,12 +1,23 @@
 """Server for Tango app."""
 
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
+app.app_context().push()
 
 
 # Replace this with routes and view functions!
+@app.route("/login")
+def login_page():
+    """Render login page"""
 
+    user = request.args.get('firstname')
+    return render_template("login.html", name=user)
+
+@app.route("/events")
+def events_page():
+
+    return render_template("events.html")    
 
 if __name__ == "__main__":
 
