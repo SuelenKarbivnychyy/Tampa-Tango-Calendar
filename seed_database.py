2de9e924ddcf048bdd31d8cd1313f1a1b87e22ba
suelenk <suelenmatosr@gmail.com>
2022-11-11 01:09:41 -0500

    created events.json and events_location.json with informations to auto seed the data base, added logic to the seed_data,py to automatically seed the data base for the events and location table

--- seed_database.py
+++ seed_database.py
@@ -18,6 +18,7 @@ model.db.create_all()
 
 
 #Load movie data from json file
+# SEEDING THE EVENT_TYPE TABLE
 with open('data/events_type.json') as file:
     events_type= json.loads(file.read())
     # print(f"####################### THIS IS THE EVENTS TYPE FROM JSON FILE {events_type}")
@@ -32,8 +33,64 @@ for event_type in events_type:
 # print(f"################################## THE LIST AFTER APPEND {events_type_in_db}")
 
 model.db.session.add_all(events_type_in_db)
-model.db.session.commit()
-# print(f"################################## AFTER COMMIT {events_type_in_db}")
+
 
 
 ##############################################################################################################################################
+# SEEDING THE LOCATION TABLE
+
+#Load movie data from json file
+with open('data/events_location.json') as location_file:
+    events_location_file= json.loads(location_file.read()) 
+
+events_location_in_db = []
+for event_location in events_location_file:
+    venue_name = event_location['venue_name']  
+    address = event_location['address']
+    city = event_location['city']
+    state = event_location['state']
+    zipcode = event_location['zipcode']
+    
+    db_event_location = crud.create_location(venue_name, address, city, state, zipcode)
+    events_location_in_db.append(db_event_location)
+
+
+
+model.db.session.add_all(events_location_in_db)
+
+###############################################################################################################################################
+#SEED THE EVENT TABLE
+
+with open('data/events.json') as events_file:
+    events= json.loads(events_file.read()) 
+
+events_in_db = []
+for event in events:
+    duration = event['duration']  
+    description = event['description']
+    date = event['date']
+    price = event['price']
+    location_id = event['location_id']
+    event_type_id = event['event_type_id']
+    
+    db_event = crud.create_event(duration, description, date, price, location_id, event_type_id)
+    events_in_db.append(db_event)
+
+
+
+model.db.session.add_all(events_in_db)
+
+
+# ###########################################################################################################################################
+#GENERATE 10 FAKES ACCOUNTS
+
+for n in range(10):
+    fname = f"{n}dima"
+    lname = f"karb{n}"
+    email = f"user{n}@test.com"  # Voila! A unique email!
+    password = "test"
+
+    user = crud.create_user(fname, lname, email, password)
+    model.db.session.add(user)
+
+model.db.session.commit()                                               #COMMIT TO THE DATABASE AT THE VERY END TO AVOID COMMIT ALL THE TIME YOU ADD NEW INF TO THE DATABASE
