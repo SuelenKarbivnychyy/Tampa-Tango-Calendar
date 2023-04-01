02cf25a690a543bfdbb4a038a43066b0275ff671
suelenk <suelenmatosr@gmail.com>
2023-03-31 21:08:04 -0400

    Created two files to separate the user and event functionality for the purpose of keep the code organized

---
+++ event_blueprint.py
@@ -0,0 +1,123 @@
+from flask import Flask, Blueprint, request, render_template, flash, session, redirect, jsonify
+from model import connect_to_db, db, Event, Location, Event_type
+import crud
+from jinja2 import StrictUndefined                                   
+from datetime import datetime 
+import send_email 
+from sqlalchemy import update  
+
+
+event_functionality_page = Blueprint('event_functionality_page', __name__, template_folder='templates')
+
+
+@event_functionality_page.route("/events")
+def show_all_events(): 
+    """View all events"""
+
+    events = crud.get_all_events()
+    rate = {}  
+
+    for event in events:
+        total_review_rate = 0
+        for review in event.reviews:
+            total_review_rate += review.rate
+        total_reviews_per_event = len(event.reviews)
+        rate_character = "⭐"
+        if total_review_rate > 0:
+            average_rate_per_event = round(total_review_rate / total_reviews_per_event)
+            rate[event.id] = rate_character * average_rate_per_event
+    
+    list_of_attendance = crud.get_all_attendance()
+    user_id = session.get("current_user")    
+    current_user_events={}
+
+    if user_id != None:    
+        attendances = crud.get_all_attendance_for_a_user(user_id)  
+        for attendance in attendances:
+            current_user_events[attendance.event_id] = "true"    
+    return render_template("events.html", events=events,
+                                        attendances=current_user_events,
+                                        attendance_number = list_of_attendance,
+                                        rate=rate
+                                        )
+
+
+@event_functionality_page.route("/events/<id>")    
+def show_event_details(id):
+    """View event details"""
+
+    user = session.get("current_user")
+    event = crud.get_event_by_id(id)
+    review = crud.get_review_by_event_and_user(event.id, user)
+    reviews_db = crud.get_all_reviews_by_event_id(event.id)    
+    return render_template("events_details.html", event = event, review = review, reviews_db = reviews_db)
+
+
+@event_functionality_page.route("/event_sign_in", methods=["POST"])
+def sign_in_for_event():
+    """Register user to attend an event"""    
+
+    event_id = request.json.get("event_id")
+    user_id = session.get('current_user')
+    if user_id == None:                                                                                  #if user not in session it will not allows user to sign in for event
+        return "Please login."
+
+    check_attendance = crud.get_attendance(event_id, user_id)        
+    if check_attendance == None:   
+        add_attendance = crud.create_attendance(event_id, user_id)
+        db.session.add(add_attendance)        
+        db.session.commit()        
+        return "You are sucessfully sign in for this event."
+    else:
+        return "You are already sign in for this event."
+
+
+@event_functionality_page.route("/sign_out", methods=["POST"])    
+def cancel_user_attendance():
+    """Cancel user attendance to an event"""   
+
+    event_id = request.json.get("event_id")
+    user_id = session['current_user'] 
+    attendance = crud.get_attendance(event_id, user_id)
+
+    if attendance != None: 
+        db.session.delete(attendance)
+        db.session.commit()
+        return "You are signed out for this event. We are sorry to let you go."
+    else:
+        return "You've signed out."      
+
+
+@event_functionality_page.route("/review", methods=["POST"])
+def create_review():
+    """Create a review"""
+
+    event_id = request.json.get('event_id')
+    user_id = session.get('current_user')    
+    user_rate = request.json.get("user_rate")
+    user_review = request.json.get("user_review")
+    review_from_db = crud.get_review_by_event_and_user(event_id, user_id)
+    
+    if review_from_db == None:    
+        new_review = crud.create_review(user_rate, user_review, event_id, user_id)
+        db.session.add(new_review)
+        db.session.commit()
+        return "true"
+    else:
+        print(f"################## review {review_from_db.comment}")
+        return "false"
+    
+
+@event_functionality_page.route("/delete_review", methods=["POST"])
+def delete_review():
+    """Delete a review"""
+    
+    review_id = request.json.get('review_id')
+    review_in_db = crud.get_review_by_id(review_id)    
+    print(f"############################# review id: [{review_in_db}]")
+    if review_in_db != None:
+        db.session.delete(review_in_db)
+        db.session.commit()
+        return "Review deleted"
+    else:
+        return "deleted"    
---
+++ user_blueprint.py
@@ -0,0 +1,98 @@
+from flask import Flask, Blueprint, request, render_template, flash, session, redirect, jsonify
+from model import connect_to_db, db, Event, Location, Event_type
+import crud
+from jinja2 import StrictUndefined                                   
+from datetime import datetime 
+import send_email 
+from sqlalchemy import update  
+
+
+user_page = Blueprint('user_page', __name__, template_folder='templates')
+
+
+@user_page.route("/validate_user_credentials", methods=["POST"])
+def validate_user_credentials():           
+    """Check if user's credentials are correct to login"""    
+   
+    email_from_input = request.json.get("email")                                      #information from browser as json
+    password = request.json.get("password")
+    
+    user_db = crud.get_user_by_email(email_from_input)                      #checking if the user's email input from the browser has any matchs at the database
+    
+    if user_db == None :                                                    #checking if user is not in db and return "no results if true"
+        return "no result"  
+   
+    user_id= user_db.id
+    user_password = user_db.password                                        #getting the password from db's user if there is a match
+    if user_password == password:                                           #giving access to user if informations macth
+        session['current_user'] = user_id
+        session["user_name"] = f"{user_db.fname} {user_db.lname}"            #setting first name and last name from session
+        is_administrator = user_db.is_adm
+        session["is_adm"] = is_administrator        
+        return "true"
+    else:
+        return "false"
+
+
+@user_page.route("/logout")
+def lougout():    
+    """Logout user from session and redirect to homepage"""    
+
+    session.pop('user_name')
+    session.pop('current_user')
+    session.pop('is_adm')
+    return redirect('/')  
+
+
+@user_page.route("/create_account") 
+def create_account():
+    """Render create account template"""   
+
+    return render_template("create_account.html") 
+
+
+@user_page.route("/validate_email", methods=["POST"])
+def validate_email():
+    """Validate email to create an account by check if the email is on database"""
+
+    email = request.json.get("email")
+    user = crud.get_user_by_email(email)
+    if user != None:
+        return "true"
+    else:
+        return "false"
+
+
+@user_page.route("/add_user_to_db", methods=["POST"])
+def add_account():
+    """Add an user account to database"""   
+
+    first_name = request.form.get("firstname")
+    last_name = request.form.get("lastname")
+    email = request.form.get("email")
+    password = request.form.get("password") 
+
+    new_user = crud.create_user(first_name, last_name, email, password)
+    db.session.add(new_user)
+    db.session.commit()     
+    return redirect("/")  
+
+
+@user_page.route("/user_profile")
+def display_user_profile():
+    """Display user's profile"""
+    
+    user = session.get("current_user")    
+    if user == None:
+        return redirect('/')
+
+    attendances = crud.get_all_attendance_for_a_user(user)
+    events = []    
+    for attendance in attendances:
+        events.append(attendance.events)    
+
+    user_reviews = crud.get_all_reviews_for_a_user(user)
+    reviews = []
+    for review in user_reviews:
+        reviews.append(review) 
+    return render_template("/user_profile.html", events=events, reviews=reviews)
